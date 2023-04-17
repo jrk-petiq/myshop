54aba056f5ea79003798f8b6e1bae6b0caa3d11f
John Kowalsky <john.kowalsky@petiq.com>
2023-04-17 12:50:20 -0600

    up through to ch 10 Extending Your Shop

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from orders.models import Order
 
+from .tasks import payment_completed
+
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -35,5 +37,7 @@ def stripe_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
--- terminal_commands.md
+++ terminal_commands.md
@@ -1,5 +1,15 @@
 # for running rabbitmq on docker
+
 `docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management`
 
 # for Stripe webhook listening to localhost
+
 `stripe listen --forward-to localhost:8000/payment/webhook/`
+
+# celery worker
+
+`celery -A myshop worker -l info`
+
+# celery flower
+
+`celery -A myshop flower`
