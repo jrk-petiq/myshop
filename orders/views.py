76ff4f7f8f6c5ccbf99648bc776dea1c1d28230b
John Kowalsky <john.kowalsky@petiq.com>
2023-04-17 10:51:14 -0600

    up through to Generation PDF invoices dynamically

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import redirect, render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from cart.cart import Cart
 
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import Order, OrderItem
 from .tasks import order_created
 
 
@@ -34,3 +35,9 @@ def order_create(request):
     return render(
         request, "orders/order_create.html", {"cart": cart, "form": form}
     )
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order_detail.html", {"order": order})
